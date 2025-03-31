74c4a0edc907bd2bec6e2a866d2bcdc6eb886875
이다은 <162148102+Amyleefiwfjj@users.noreply.github.com>
2025-03-31 15:48:32 +0900

    s

--- mainPage.js
+++ mainPage.js
@@ -22,27 +22,6 @@ function draw() {
     image(images[i], currentX, y + i *60,2700,60); 
   }
 }
-window.addEventListener('load', function() {
-  // 로딩 화면 숨기기
-  const loadingScreen = document.getElementById('loading-screen');
-  const content = document.getElementById('content');
-
-  // 로딩 화면을 숨기고 콘텐츠를 표시
-  loadingScreen.style.display = 'none';
-  content.style.display = 'block';
-});
-function mouseWheel(event) {
-  scrollCount += Math.sign(event.delta);  // event.delta는 음수(위로 스크롤)와 양수(아래로 스크롤) 값을 가짐
-  if (Math.abs(scrollCount) >= 5) {
-    addNewElements();  // 새로운 요소 추가 함수 호출
-    scrollCount = 0;   // 카운트 초기화
-  }
-  if (scrollCount>=0){
-  for (let i = 0; i < images.length; i++) {
-    deltaX[i] -= event.delta * 0.15 * (i + 1); 
-  }
-}
-}
 function addNewElements() {
   // 새 요소를 동적으로 추가 (HTML에 추가)
   const newDiv = createDiv();  // p5.js에서 HTML div 요소 생성
